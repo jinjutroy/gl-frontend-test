11cea93820219b6c7a73febc1856c415874ff3f5
Ngoc Yen <ngocyen.bui@estuary.solutions>
2025-05-12 12:10:01 +0700

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
-  basePath: "/gl-frontend-test",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
